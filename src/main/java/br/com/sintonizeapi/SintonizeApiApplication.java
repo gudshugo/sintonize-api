1771c6bde4985e38f1e9a075196b299007870f67
gudshugo <gudshugo@Hugos-MacBook-Pro.local>
2022-10-27 00:15:13 -0400

    Initial commit

---
+++ src/main/java/br/com/sintonizeapi/SintonizeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.sintonizeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SintonizeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SintonizeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sintonizeapi/SintonizeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sintonizeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SintonizeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
